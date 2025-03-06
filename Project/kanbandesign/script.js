2b9bf171ab16dae763e29f932a820b93a1a16a1c
Rakesh Bhetariya <rakeshbhetariya29@gmail.com>
2025-03-06 14:45:03 +0530

    you can edit and delete your task

--- Project/kanbandesign/script.js
+++ Project/kanbandesign/script.js
@@ -4,7 +4,7 @@ const todoBoard = document.getElementById("todo-board");
 
 const allBoard = document.querySelectorAll(".board");
 
-const allItems = document.querySelectorAll(".item");
+const allItems = document.querySelectorAll(".items");
 
 // when i used getElementByClassName so i can not use foreach but when i use query selector so i can use foreach so why this is happen means find the reaon behind the error
 
@@ -21,14 +21,30 @@ addTaskBtn.addEventListener("click", () => {
   const input = prompt("Enter a task");
   if (!input) return;
 
-  const taskCard = document.createElement("p");
+  const divElemet = document.createElement("div");
+  divElemet.classList.add("items");
+
+  const deleeteButton = document.createElement("button");
+  deleeteButton.classList.add("delete-btn");
+  const icon = document.createElement("i");
+
+  // Add FontAwesome classes to the <i> element
+  icon.classList.add("fa", "fa-trash");
+
+  // this how you can add style using js
+  icon.style.fontSize = "24px";
+  deleeteButton.appendChild(icon);
+
+  const taskCard = document.createElement("input");
   taskCard.classList.add("item");
-  taskCard.innerText = input;
+  // taskCard.innerText = input;
+  taskCard.value = input;
   taskCard.setAttribute("draggable", true);
 
-  addFlayingClass(taskCard);
-
-  todoBoard.appendChild(taskCard);
+  addFlayingClass(divElemet);
+  divElemet.appendChild(taskCard);
+  divElemet.appendChild(deleeteButton);
+  todoBoard.appendChild(divElemet);
 });
 
 allItems.forEach((item) => {
@@ -42,6 +58,14 @@ allBoard.forEach((board) => {
   });
 });
 
+function editTask(element, type) {
+  let input = element.value;
+
+  if (!input) {
+    alert("Please don't leave empty item");
+    element.value = "You remove this task";
+  }
+}
 
 /*  this is are the functionality i want to add 
 - make transition smooth 
@@ -55,4 +79,3 @@ allBoard.forEach((board) => {
 - Time on every task  
 
 */
-
